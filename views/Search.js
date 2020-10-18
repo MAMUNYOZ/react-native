4f4a6dba93812f5d6a7b5ec3fb0bbcc16ec8ac4d
MAMUNYOZ <milangito@hotmail.com>
2020-10-18 15:49:28 +0200

    V0.5.5 - Buscador

--- views/Search.js
+++ views/Search.js
@@ -1,10 +1,60 @@
-import React from "react";
-import {View, Text} from 'react-native';
+import React, {useContext, useEffect, useState} from 'react';
+import {View} from 'react-native';
+import {
+  Container,
+  Content,
+  Item,
+  Input,
+  Icon,
+  List,
+  H1
+} from 'native-base';
 
-export default function Search() {
-    return (
-        <View>
-            <Text>Estamos en BUSCADOR</Text>
+import {Product} from '../components/product';
+
+import globalStyles from '../styles/global';
+
+import ServerContext from '../context/server/serverContext';
+
+const Search = () => {
+  const {products} = useContext(ServerContext);
+
+  const [seach, setSeach] = useState('');
+  const [seachResult, setResult] = useState('');
+
+  const getProducts = (productsList, description) => {    
+    if (description === '') {
+      return [];
+    }
+    description = description.toLocaleLowerCase();
+    return productsList.filter((product) =>
+      product.description.toLocaleLowerCase().includes(description),
+    ); 
+  };
+
+  useEffect(() => {
+    setResult(getProducts(products, seach));
+  }, [seach]);
+
+  return (
+    <Container style={globalStyles.container}>
+      <Content style={globalStyles.content}>
+      <H1 style={globalStyles.title}> <Icon name="md-wine"/> Buscador de Productos</H1>
+        <View searchBar rounded>
+          <Item>
+            <Icon name="ios-search" />
+            <Input placeholder="Palabra a buscar" 
+            onChangeText= {(words) => setSeach(words)}/>
+          </Item>
         </View>
-    )
-}
+        <List>
+          {seachResult.map((product, index) => {
+            return <Product {...product} key={index}/>;
+          })}
+        </List>
+      </Content>
+    </Container>
+  );
+};
+
+export default Search;
